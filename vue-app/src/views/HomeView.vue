9a71075266e59a777fb7004e5d12bf77eaa19a6d
Edittia <aittidejonveri04@gmail.com>
2023-03-28 08:33:19 +0700

    first commit

---
+++ vue-app/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <div class="container">
+        <div class="row">
+          <div class="col text-center">
+            <img style="width: 25%;" src="https://www.mushaf.id/sources/img/logo-lg.png">
+          </div>       
+        </div>
+        <div class="row">
+          <div class="col">
+            <p style="color: #e7e9ea;">surah</p>
+          </div>
+        </div>
+    </div>
+
+
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
